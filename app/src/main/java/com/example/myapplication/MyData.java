2aaee7870e6a0ef541fa0c289c686b226a48d3e0
Lucille3007 <lusil160@gmail.com>
2024-01-29 16:39:44 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/MyData.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+
+public class MyData {
+
+    static String[] nameArray = {"Blake Ellender Lively", "Leighton Marissa Meester", "Penn Badgley", "Chace Crawford", "Taylor Momsen",
+                                "Ed Westwick", "Kelly Rutherford","Matthew Settle", "Jessica Szohr", "Connor Paolo"};
+
+//   ##### The description of every role: #####
+    static String Serena = "Role name: Serena van der Woodsen.\nSerena is featured on the blog of the series' mysterious narrator,\"Gossip Girl\"." +
+            "Serena is known as the 'it girl of Manhattan' and is a character that appears to easily get whatever she wants.";
+    static String Blair ="Role name: Blair Cornelia Waldorf.\nDue to her position as queen bee of Manhattan's social scene," +
+            "Blair's actions and relations are under constant scrutiny from the mysterious Gossip Girl, a popular blogger.";
+    static String Daniel ="Role name: Daniel Humphrey.\nComes from an artistic and musically-inclined family." +
+            "The character pursued his education at St. Jude's Preparatory School on the West side as a scholarship student.";
+    static String Nathaniel = "Role name: Nathaniel Archibald.\nNate is a lacrosse player at the elite St. Jude's School for Boys." +
+            "He has been dating Blair since middle school. However, Nate have deep, unresolved feelings for Serena.";
+    static String Jenny  = "Role name: Jenny Humphrey.\nShe has an older brother, Dan Humphrey." +
+            "Jenny is a student at the Constance Billard School for Girls, an elite, all-girls school on the Upper East Side " +
+            "that Serena and Blair also attend.";
+    static String Chuck = "Role name: Chuck Bass.\nHe is noted for his financial ambition, hedonism and personal style." +
+            "Chuck has been best friends with Nate, Serena and Blair, who is his main romantic interest throughout the series";
+    static String Lily = "Role name: Lily van der Woodsen.\nThe mother to Serena and Eric van der Woodsen." +
+            "A socialite, art collector, and philanthropist. She and her husband are on the boards of all major charities and art organizations in the city.";
+    static String Rufus = "Role name: Rufus Humphrey.\nRufus is the father of Dan and Jenny Humphrey, and the ex-husband of Alison Humphrey and Lily van der Woodsen." +
+            "An editor of Beat poets, rock musician who has never been published himself.";
+    static String Vanessa = "Role name: Vanessa Abrams.\nVanessa is a filmmaker and Constance student on scholarship. Her dream is to attend NYU and major in film." +
+            "Her hippie artist live in Vermont but allowed her to move to Brooklyn, to live with her sister.";
+    static String Eric = "Role name: Eric van der Woodsen.\nThe elder brother of Serena.He did have a bigger part in the \"Gossip Girl\" prequel," +
+            " his suicide attempt becomes the catalyst for Serena's homecoming.";
+
+//    ##### All the descriptions: #####
+    static String[] descriptionArray = {
+            Serena, Blair, Daniel, Nathaniel, Jenny,
+            Chuck, Lily, Rufus, Vanessa, Eric};
+
+//    ##### All the photos: #####
+    static Integer[] drawableArray = {R.drawable.blake_l, R.drawable.leighton_m, R.drawable.penn_b,
+            R.drawable.chace_c, R.drawable.taylor_m, R.drawable.ed_w, R.drawable.kelly_r,
+            R.drawable.matthew_s, R.drawable.jessica_s,R.drawable.connor_p};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
